3595fcd92283779e1bfeee4e5a75d789369ff951
Zeeshux7860 <ctechviral@gmail.com>
2023-12-26 09:32:36 +0530

    ok

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -42,30 +42,40 @@ class _PayUIState extends State<PayUI> {
   // final _phonepeGatewayPlugin = PhonepeGateway();
   @override
   void initState() {
+    // PhonpePaymentGateway.instance.init(
+    //     config: PhonePeConfig(
+    //         redirectUrl: "http://127.0.0.1/test/view",
+    //         baseUrl: "http://127.0.0.1/test",
+    //         appName: "PhonePe Test App",
+    //         callBackUrl:
+    //             "https://webhook.site/845cb8cc-5d74-4494-95ea-3003c9c518ab",
+    //         merchanId: "************",
+    //         saltIndex: 1,
+    //         saltKey: "********-****-****-****-***********"));
     PhonpePaymentGateway.instance.init(
         config: PhonePeConfig(
-            redirectUrl: "http://127.0.0.1/test/view",
-            baseUrl: "http://127.0.0.1/test",
+            redirectUrl: "https://api-v1.voiceclub.app/test/view",
+            baseUrl: "https://api-v1.voiceclub.app/test",
             appName: "PhonePe Test App",
             callBackUrl:
                 "https://webhook.site/845cb8cc-5d74-4494-95ea-3003c9c518ab",
-            merchanId: "************",
+            merchanId: "VOICECLUBONLINE",
             saltIndex: 1,
-            saltKey: "********-****-****-****-***********"));
+            saltKey: "a9e8cbaf-c914-48ec-80db-3b9f19e745f1"));
     PhonpePaymentGateway.instance.handlerCancelled(
       (value) {
-        debugPrint("Cancelled :${jsonEncode(value.toJson())}");
+        showPaymentAlertDialog("Cancelled :${jsonEncode(value.toJson())}");
       },
     );
     PhonpePaymentGateway.instance.handlerFailed(
       (value) {
-        debugPrint("Failed :${jsonEncode(value.toJson())}");
+        showPaymentAlertDialog("Failed :${jsonEncode(value.toJson())}");
       },
     );
 
     PhonpePaymentGateway.instance.handlerSuccess(
       (value) {
-        debugPrint("Success :${jsonEncode(value.toJson())}");
+        showPaymentAlertDialog("Success :${jsonEncode(value.toJson())}");
       },
     );
     super.initState();
@@ -95,4 +105,27 @@ class _PayUIState extends State<PayUI> {
           child: const Icon(Icons.add),
         ));
   }
+
+  void showPaymentAlertDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Payment Status"),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
+
+// Function to show the payment alert dialog
+
